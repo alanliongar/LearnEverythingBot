dfd1d74abb3d7a2b6139021487ba104e7d8df3af
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2025-08-20 12:14:42 -0300

    Primeira reuniao

---
+++ app/src/main/java/com/example/learneverythingbot/NotasParaCorrecao.kt
@@ -0,0 +1,10 @@
+/*
+Diário:
+20/08/2025 -
+Definido na daily de hoje: Faremos um MVP simples com 1 funcionalidade
+1 - Oscar trabalhará na primeira tela de boas vindas (design E layout)
+2 - Juliana trabalhará na segunda tela do mvp (design e layout)
+        Vale deixar anotado que Juliana e Oscar se comunicarao diretamente para definirem cores do tema, letras, etc... o tema inteiro.
+3 - Alan trabalhará na integracao com a API
+Timeline estimada para a entrega da funcionalidade simples: 1 dia. data limite: 22/08/2025.
+*/
