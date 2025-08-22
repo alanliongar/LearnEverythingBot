49c1eacd5ef2d215f992172350adfe20085aea1a
Alan <alan.lucindo.gomes@alumni.usp.br>
2025-08-22 14:31:20 -0300

    3 - new annotations to diary

--- app/src/main/java/com/example/learneverythingbot/NotasParaCorrecao.kt
+++ app/src/main/java/com/example/learneverythingbot/NotasParaCorrecao.kt
@@ -1,6 +1,7 @@
+package com.example.learneverythingbot
+//Diário
 /*
-Diário:
-20/08/2025 -
+Dia 20/08/2025
 Definido na daily de hoje: Faremos um MVP simples com 1 funcionalidade
 1 - Oscar trabalhará na primeira tela de boas vindas (design E layout)
 2 - Juliana trabalhará na segunda tela do mvp (design e layout)
@@ -8,3 +9,26 @@ Definido na daily de hoje: Faremos um MVP simples com 1 funcionalidade
 3 - Alan trabalhará na integracao com a API
 Timeline estimada para a entrega da funcionalidade simples: 1 dia. data limite: 22/08/2025.
 */
+
+/*
+Dia 21/08/2025
+1 - Realinhamento simples entre Alan e Oscar, a respeito da tela e pré-brainstorm sobre as proximas funcionalidades.
+2 - Oscar segue interessado em escrever testes unitários.
+
+Dia 22/08/2025
+1 - Alan encerrou a integração com a openAi
+2 - Oscar encerrou a criação da primeira tela
+
+Próximos passos:
+3 - Juliana encerrar a tela de chat e a navegação do compose (Força, Juuu);
+4 - Oscar fez bastante coisa para o nosso MVP, a parte dele está ok por enquanto;
+5 - Alan ficou responsável pela criação do viewmodel, mas ficou acordado que faremos isso em equipe, pois a Juliana e Oscar querem acompanhar o processo (deixa co pai)
+
+Obs importantes:
+6 - O André Morientes se apresentou, e ficou ciente de onde estamos no projeto (board trello + github).
+    Ele vai acompanhar nossa finalização do MVP sem colocar a mão na massa agora. Trabalhará na melhoria e
+    implementação de novas funcionalidades a partir de segunda-feira, dia 25/08/2025.
+7 - Definiremos um novo horário pra daily.
+*/
+
+
