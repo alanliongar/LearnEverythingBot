d24691e998bca7791bd74dd5ee74e24725099661
Alan <alan.lucindo.gomes@alumni.usp.br>
2025-08-27 18:25:00 -0300

    5 - Refactoring into packages - clean architecture

--- app/src/main/java/com/example/learneverythingbot/MainActivity.kt
+++ app/src/main/java/com/example/learneverythingbot/MainActivity.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.example.learneverythingbot.presentation.ChatViewModel
 import com.example.learneverythingbot.presentation.screen.ui.theme.LearnEverythingBotTheme
 import dagger.hilt.android.AndroidEntryPoint
 
